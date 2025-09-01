7a10665b70cd00956ded5fbbbb83c5a9c18e9afd
Justine Agcanas <agcanasjustine1@gmail.com>
2025-09-01 20:41:05 +0800

    feat: added mysql database integration

--- registration_form/process.php
+++ registration_form/process.php
@@ -1,46 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Process</title>
     <style>
-        table, td, th {
+        table,
+        td,
+        th {
             border: 1px solid #212121;
             border-collapse: collapse;
             padding: 5px;
             font-size: 16px;
         }
     </style>
+
 <body>
-   <table>
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Age</th>
-            <th>Gender</th>
-            <th>Favorite Color</th>
-            <th>Message</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <?php
-                $name = $_POST["name"];
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Age</th>
+                <th>Gender</th>
+                <th>Favorite Color</th>
+                <th>Message</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <?php
+                $fullname = $_POST["fullname"];
                 $email = $_POST["email"];
                 $age = $_POST["age"];
                 $gender = $_POST["gender"];
-                $fcolor = $_POST["fcolor"];
+                $color = $_POST["color"];
                 $message = $_POST["message"];
 
-                echo "<td>" . htmlspecialchars($name) . "</td>";
+                echo "<td>" . htmlspecialchars($fullname) . "</td>";
                 echo "<td>" . htmlspecialchars($email) . "</td>";
                 echo "<td>" . htmlspecialchars($age) . "</td>";
                 echo "<td>" . htmlspecialchars($gender) . "</td>";
-                echo "<td>" . htmlspecialchars($fcolor) . "</td>";
+                echo "<td>" . htmlspecialchars($color) . "</td>";
                 echo "<td>" . htmlspecialchars($message) . "</td>";
-            ?>
-        </tr>
-    </tbody>
-   </table> 
+                ?>
+            </tr>
+        </tbody>
+    </table>
 </body>
+
 </html>
